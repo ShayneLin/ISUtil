6e7d74a9429b3dc93a3bbb277b8b81ec597f19fe
ShayneLin <linjankangshun@163.com>
2018-01-15 17:26:17 +0800

    Mac的常量
    定义了一些MAC算法的常量字段

---
+++ src/main/java/com/shayne/isutil/constant/MacConst.java
@@ -0,0 +1,22 @@
+package com.shayne.isutil.constant;
+
+/**
+ * 定义一些MAC算法的常量名字
+ * 
+ * @author Administrator
+ *
+ */
+public class MacConst {
+	// ============SUN=========================
+	public static final String HmacMD5 = "HmacMD5";
+	public static final String HmacSHA1 = "HmacSHA1";
+	public static final String HmacSHA256 = "HmacSHA256";
+	public static final String HmacSHA384 = "HmacSHA384";
+	public static final String HmacSHA512 = "HmacSHA512";
+
+	// ==================BouncyCastle=============================
+	public static final String HmacMD2 = "HmacMD2";
+	public static final String HmacMD2Hex = "HmacMD2Hex";
+	public static final String HmacMD4 = "HmacMD4";
+	public static final String HmacSHA224 = "HmacSHA224";
+}
